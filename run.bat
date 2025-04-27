766c678aba0b7b8c80383dc9a5b6e65c6a16fb4f
theotimeturolla <151567990+theotimeturolla@users.noreply.github.com>
2025-04-27 22:34:54 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,29 +1,4 @@
 @echo off
-title Assistant Culinaire IA
-echo.
-echo ================================
-echo     🍽️ Assistant Culinaire IA
-echo ================================
-echo.
-echo Que veux-tu lancer ?
-echo 1. Assistant en ligne de commande (PROJETV2.py)
-echo 2. Interface graphique (interface2.py)
-echo.
-
-set /p choix=Fais ton choix (1 ou 2) : 
-
-echo.
-echo === Activation de l’environnement virtuel ===
-call venv\Scripts\activate.bat
-
-if "%choix%"=="1" (
-    echo === Lancement du bot en terminal ===
-    python PROJETV2.py
-) else if "%choix%"=="2" (
-    echo === Lancement de l'interface graphique ===
-    python interface2.py
-) else (
-    echo Choix invalide. Relance le script et tape 1 ou 2.
-)
-
+cd /d "%~dp0"
+python interface2.py
 pause
